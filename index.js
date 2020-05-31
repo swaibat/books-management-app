e3fbdf737152746c786097fb9b596828beb19109
swaibu <sswaib11@gmail.com>
2020-05-31 05:34:57 +0300

    protect books routes using JwT

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './app/routes/index';
 
 const app = express();
 app.use(cors());
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/api/v1', routes);
-// should be added after all routes
+
 app.use('*', (req, res) => {
   res.status(404).json({
     status: 404,
     error: `${req.method}=${req.protocol}://${req.headers.host}${req.originalUrl} not found`,
   });
 });
-// HANDLING BODY PARSER ERRORS
+
 app.use((error, req, res) => {
   res.status(400).json({
     status: 400,
