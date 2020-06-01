ac077c00cd896700916980de34e49e168d81a5af
swaibu <sswaib11@gmail.com>
2020-06-01 17:04:28 +0300

    implement image upload using Cloudinary

--- app/routes/book.route.js
+++ app/routes/book.route.js
@@ -1,15 +1,20 @@
 import express from 'express';
+import fileUpload from 'express-fileupload';
 import Book from '../controller/book.controller';
 import BookMiddleware from '../middleware/book.middleware';
 import UserMiddleware from '../middleware';
 import validate from '../helpers/validator';
+import helper from '../helpers/cloudinary';
 
 const app = express.Router();
-
-app.post('/', UserMiddleware.verifyToken, validate.book, BookMiddleware.checkIfExists, Book.create);
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/',
+}));
+app.post('/', UserMiddleware.verifyToken, helper.imageUpload, validate.book, BookMiddleware.checkIfExists, Book.create);
+app.patch('/:id', UserMiddleware.verifyToken, helper.imageUpload, validate.book, BookMiddleware.getBookById, Book.update);
 app.get('/', Book.getAll);
 app.get('/:id', UserMiddleware.verifyToken, BookMiddleware.getBookById, Book.getOne);
-app.patch('/:id', UserMiddleware.verifyToken, validate.book, BookMiddleware.getBookById, Book.update);
 app.delete('/:id', UserMiddleware.verifyToken, BookMiddleware.getBookById, Book.delete);
 
 export default app;
--- test/books.spec.js
+++ test/books.spec.js
@@ -12,12 +12,12 @@ describe('Books CRUD Ops', () => {
     chai.request(app)
       .post('/api/v1/books')
       .set('Authorization', helper.createToken({ email: 'demo@gmail.com', username: 'demo' }).token)
-      .send({
+      .field({
         title: 'Rich Dad Poor Dad',
         author: 'Robert Kiyosaki',
-        isbn: '978-3-16-148410-12',
-        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Rich_Dad_Poor_Dad.jpg/220px-Rich_Dad_Poor_Dad.jpg'
+        isbn: '978-3-16-148410-12'
       })
+      .attach('image', './test/test.jpg')
       .end((err, res) => {
         res.should.have.status(201);
         res.body.should.be.a('object');
@@ -25,16 +25,16 @@ describe('Books CRUD Ops', () => {
         done();
       });
   });
-  it('Should create book successfully', (done) => {
+  it('Should create errotr 400', (done) => {
     chai.request(app)
       .post('/api/v1/books')
       .set('Authorization', helper.createToken({ email: 'demo@gmail.com', username: 'demo' }).token)
-      .send({
+      .field({
         title: '',
         author: 'Robert Kiyosaki',
-        isbn: '978-3-16-148410-12',
-        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Rich_Dad_Poor_Dad.jpg/220px-Rich_Dad_Poor_Dad.jpg'
+        isbn: '978-3-16-148410-12'
       })
+      .attach('image', './test/test.jpg')
       .end((err, res) => {
         res.should.have.status(400);
         res.body.should.be.a('object');
@@ -45,12 +45,12 @@ describe('Books CRUD Ops', () => {
     chai.request(app)
       .post('/api/v1/books')
       .set('Authorization', helper.createToken({ email: 'demo@gmail.com', username: 'demo' }).token)
-      .send({
+      .field({
         title: 'Rich Dad Poor Dad',
         author: 'Robert Kiyosaki',
-        isbn: '978-3-16-148410-12',
-        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Rich_Dad_Poor_Dad.jpg/220px-Rich_Dad_Poor_Dad.jpg'
+        isbn: '978-3-16-148410-12'
       })
+      .attach('image', './test/test.jpg')
       .end((err, res) => {
         res.should.have.status(409);
         res.body.should.be.a('object');
@@ -62,16 +62,32 @@ describe('Books CRUD Ops', () => {
     chai.request(app)
       .patch('/api/v1/books/1')
       .set('Authorization', helper.createToken({ email: 'demo@gmail.com', username: 'demo' }).token)
-      .send({
-        title: 'Rich Dad Poor Dad 3',
+      .field({
+        title: 'Rich Dad Poor Dad',
         author: 'Robert Kiyosaki',
-        isbn: '978-3-16-148410-12',
-        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Rich_Dad_Poor_Dad.jpg/220px-Rich_Dad_Poor_Dad.jpg'
+        isbn: '978-3-16-148410-12'
       })
+      .attach('image', './test/test.jpg')
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
-        res.body.data.should.have.property('title').eql('Rich Dad Poor Dad 3');
+        res.body.data.should.have.property('title').eql('Rich Dad Poor Dad');
+        done();
+      });
+  });
+  it('Should update book successfully', (done) => {
+    chai.request(app)
+      .patch('/api/v1/books/1')
+      .field({
+        title: 'Rich Dad Poor Dad',
+        author: 'Robert Kiyosaki',
+        isbn: '978-3-16-148410-12'
+      })
+      .attach('image', './test/test.jpg')
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.be.a('object');
+        res.body.should.have.property('message').eql('provide token to get access');
         done();
       });
   });
@@ -91,7 +107,7 @@ describe('Books CRUD Ops', () => {
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
-        res.body.data.should.have.property('title').eql('Rich Dad Poor Dad 3');
+        res.body.data.should.have.property('title').eql('Rich Dad Poor Dad');
         done();
       });
   });
